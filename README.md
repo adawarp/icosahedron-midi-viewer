29cf6115fffb09646bf0d36b4cab0d4bb6a04ad2
azamiya <t.adaniya0624@gmail.com>
2021-05-29 20:00:28 +0900

    feat: add prettier and husky

---
+++ README.md
@@ -0,0 +1,8 @@
+# Icosahedron MIDI Viewer
+
+## What is Icosahedron MIDI Viewer ?
+
+- 正二十面体の各頂点に音を割り振り、MIDIで様々な直線や図形、立体を表現します
+
+## How to play ?
+- 使用方法をここに明記します
---
+++ js/main.js
@@ -0,0 +1,9 @@
+const main = () => {
+  console.warn('hello main.js');
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  main();
+
+  console.log('hoge');
+});

39582a8f59e029873ea762ae4d8d6369e09cf725
Masanori Enoki <enoki@adawarp.com>
2021-05-31 19:05:32 +0900

    feat: canvas created

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 const main = () => {
-  console.warn('hello main.js');
+  window.addEventListener('load', init);
+  function init() {
+    const width = 960;
+    const height = 540;
+    const renderer = new THREE.WebGLRenderer({
+      canvas: document.querySelector('#myCanvas'),
+    });
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(width, height);
+
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(45, width / height);
+    camera.position.set(50, 50, 50);
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    renderer.render(scene, camera);
+  }
 };
 
 document.addEventListener('DOMContentLoaded', () => {
